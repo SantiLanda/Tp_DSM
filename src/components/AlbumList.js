0f3b59e9b2cd0c3672786c817d25968d95be11ad
Maxi Saleh <MaxiSaleh@outlook.com>
2022-11-03 00:37:54 -0300

    albumList a Functional

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,46 @@ import {ScrollView, Text, View} from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
+function Album(props) {
+  const [photoset, setState] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1'
+      )
+      .then((response) => {
+        setState(response.data.photosets.photoset);
+      });
+  })
+
+funcion renderAlbums(){
+  rreturn photos.map((album) => (
+    <AlbumDetail
+      navigation={navigation}
+      key={album.id}
+      title={album.title._content}
+      albumId={album.id}
+    />
+  ));
+};
+
+if (!photoset) {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Loading...</Text>
+    </View>
+  );
+}
+return (
+  <View style={{flex: 1}}>
+    <ScrollView>{renderAlbums()}</ScrollView>
+  </View>
+);
+} 
+
+export default AlbumList;
+
+/*/
 class AlbumList extends Component {
   state = {photoset: null};
 
