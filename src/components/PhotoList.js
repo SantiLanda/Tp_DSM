e522fa4200464edfe8862dd122e7ddd780331b1c
Santiago Landa Valle <santi.land4@gmail.com>
2022-11-03 00:22:28 -0300

    photolist cambiado de clase a funcional

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {ScrollView, Text, View} from 'react-native';
 import axios from 'axios';
 import PhotoDetail from './PhotoDetail';
 
+/*
 class PhotoList extends Component {
   state = {photos: null};
 
@@ -44,5 +45,42 @@ class PhotoList extends Component {
     );
   }
 }
+*/
+
+function PhotoList() {
+  const [photos, setPhotos] = useState(null)
+  useEffect(()=> {
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${this.props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`,
+      )
+      .then((response) =>
+        setPhotos(response.data.photoset.photo),
+      );
+  })
+
+  function renderAlbums() {
+    return photos.map((photo) => (
+      <PhotoDetail
+        key={photo.title}
+        title={photo.title}
+        imageUrl={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+      />
+    ));
+  };
+  
+  if (!photos) {
+    return (
+      <View style={{flex: 1}}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+  return (
+    <View style={{flex: 1}}>
+      <ScrollView>{renderAlbums()}</ScrollView>
+    </View>
+  );
+} 
 
 export default PhotoList;
